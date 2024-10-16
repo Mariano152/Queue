e13444457637b36161946f35ff30769ae3838664
Mariano152 <a01639413@tec.mx>
2024-10-15 22:00:16 -0600

    first commit

---
+++ Queue.h
@@ -0,0 +1,27 @@
+//
+// Created by maria on 10/15/2024.
+//
+#ifndef QUEUE_H
+#define QUEUE_H
+
+
+
+class Queue {
+private:
+    int front;
+    int rear;
+    int size;
+    int* queue;
+public:
+    Queue(int capacity);
+    bool isEmpty();
+    bool isFull();
+    void enqueue(int value);
+    void dequeue();
+    int peek();
+    ~Queue();
+};
+
+
+
+#endif //QUEUE_H
